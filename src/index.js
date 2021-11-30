f07143a412082729c0bc9b86abb391c6d26c5729
Foxx <5180273+FoxxD3V@users.noreply.github.com>
2021-12-01 00:48:39 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const rest = require('./spot/rest');
+const rest = require('./spot/stream');
+const rest = require('./spot/userStream');
 
 module.exports = {
   rest,
+  stream,
+  userStream,
   error: (err) => {
     return typeof err.response.data != 'undefined' ? `${err.response.data.code}: ${err.response.data.msg}` : err;
   }
